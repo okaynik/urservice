99bb9264139f1145756cf6c6cd9dc4651b138694
Jack Ditto <jack@Jacks-MacBook-Pro.local>
2021-02-23 21:20:52 -0500

    Add .ENV for security

--- urservice/settings.py
+++ urservice/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7@b%$4av*^jnb#+!^=li-2pps*&mjmxzh4yr^kz7m^*w!497m('
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,12 +81,13 @@ WSGI_APPLICATION = 'urservice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
-        'USER': 'admin',
-        'PASSWORD': 'jackiscool',
+        'USER': os.getenv("DB_USERNAME"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': 'ur-service-db.cogzd8hec5ds.us-east-1.rds.amazonaws.com',
         'PORT': '3306'
     }
